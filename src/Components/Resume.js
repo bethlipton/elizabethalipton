a6ad709f7e2895c650fc492b30df4e4d837d3d5c
Beth <lipton.elizabeth@gmail.com>
2020-05-26 18:17:53 -0400

    added certifications section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,33 +5,39 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
-        var education = this.props.data.education.map(function (education) {
+      var education = this.props.data.education.map(function (education) {
             return (<div key={education.school}><h3>{education.school}</h3>
                 <p className="info">{education.degree} <span></span><em className="date">{education.graduated}</em></p>
                 <p>{education.description}</p></div>
             )
         });
-        var work = this.props.data.work.map(function (work) {
-            return (<div key={work.company}><h3>{work.company}</h3>
-                <p className="info">{work.title}<span></span> <em className="date">{work.years}</em></p>
-                <ul className="description">
-                    <li>{work.description11}</li>
-                    <li>{work.description12}</li>
-                    <li>{work.description13}</li>
-                    <li>{work.description14}</li>
-                    <li>{work.description15}</li>
-                </ul>
-                <p className="info">{work.title2}<span></span> <em className="date">{work.years2}</em></p>
-                <ul className="description">
-                    <li>{work.description21}</li>
-                    <li>{work.description22}</li>
-                    <li>{work.description23}</li>
-                    <li>{work.description24}</li>
-                    <li>{work.description25}</li>
-                </ul>
-            </div>
+      var certifications = this.props.data.certifications.map(function (certifications) {
+            return (<div key={certifications.certification}><h3>{certifications.certification}</h3>
+                <p className="info">{certifications.authority} <span></span><em className="date">{certifications.issued}</em></p>
+                <p>{certifications.description}</p></div>
             )
         });
+      var work = this.props.data.work.map(function (work) {
+          return (<div key={work.company}><h3>{work.company}</h3>
+              <p className="info">{work.title}<span></span> <em className="date">{work.years}</em></p>
+              <ul className="description">
+                  <li>{work.description11}</li>
+                  <li>{work.description12}</li>
+                  <li>{work.description13}</li>
+                  <li>{work.description14}</li>
+                  <li>{work.description15}</li>
+              </ul>
+              <p className="info">{work.title2}<span></span> <em className="date">{work.years2}</em></p>
+              <ul className="description">
+                  <li>{work.description21}</li>
+                  <li>{work.description22}</li>
+                  <li>{work.description23}</li>
+                  <li>{work.description24}</li>
+                  <li>{work.description25}</li>
+              </ul>
+          </div>
+          )
+      });
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
         return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
@@ -61,6 +67,21 @@ class Resume extends Component {
             <div className="row item">
                <div className="twelve columns">
                  {education}
+               </div>
+            </div>
+         </div>
+      </div>
+
+
+      <div className="row education">
+         <div className="three columns header-col">
+            <h1><span>Certifications</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+            <div className="row item">
+               <div className="twelve columns">
+                 {certifications}
                </div>
             </div>
          </div>
