eb4b5ef8a7671fe645ee8a66587ff4308aaf6a1a
bethlipton <lipton.elizabeth@gmail.com>
2022-11-30 23:40:37 -0500

    updated resume pdf, new activities image, fonts

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Elizabeth Lipton</li>
+              <li>&copy; Copyright 2022 Elizabeth Lipton</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
